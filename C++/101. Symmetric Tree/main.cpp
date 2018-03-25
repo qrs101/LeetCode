459b3564cafa52ff12e5e5604ac23b1737cadaee
qrs101 <qrs101@outlook.com>
2018-03-25 20:49:48 +0800

    commit 101

---
+++ C++/101. Symmetric Tree/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr)
+            return true;
+        return helper(root->left, root->right);
+    }
+
+    bool helper(TreeNode* r1, TreeNode* r2) {
+        if (r1 == nullptr && r2 == nullptr)
+            return true;
+        else if ((r1 != nullptr && r2 == nullptr) || (r1 == nullptr && r2 != nullptr))
+            return false;
+        else if (r1->val != r2->val)
+            return false;
+        else {
+            if (helper(r1->left, r2->right) && helper(r1->right, r2->left))
+                return true;
+            else
+                return false;
+        }
+    }
+};
