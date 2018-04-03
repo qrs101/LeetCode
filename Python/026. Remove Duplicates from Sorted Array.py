840fb36504273ca8ae84dce2d7010e55e565ad2b
qrs101 <qrs101@outlook.com>
2018-04-03 17:42:54 +0800

    commit Python 026

---
+++ Python/026. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        i = 0
+        j = 1
+        while j < n:
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+            j += 1
+        return i + 1
