213fd3f241f2d05fca04338d30f3647c76065410
qrs101 <qrs101@outlook.com>
2018-04-09 16:41:34 +0800

    modify 121

--- C++/121. Best Time to Buy and Sell Stock/main.cpp
+++ C++/121. Best Time to Buy and Sell Stock/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <vector>
-#include <map>
 using namespace std;
 
 class Solution {
 public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        if (prices.size() == 0)
+            return ans;
+        int min = prices[0], max = prices[0];
+        for (int i = 1; i < int(prices.size()); i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+                max = prices[i];
+            } else if (prices[i] > max) {
+                max = prices[i];
+            }
+            if (max - min > ans)
+                ans = max - min;
+        }
+        return ans;
+    }
+
+    /* HashTable
     int maxProfit(vector<int>& prices) {
         map<int, int> M;
         int ans = 0;
@@ -24,4 +42,5 @@ public:
         }
         return ans;
     }
+    */
 };
