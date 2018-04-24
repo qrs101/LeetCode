440f10cfaf7a98cb1a5f678add1dad0b74737bb0
qrs101 <qrs101@outlook.com>
2018-04-24 20:15:49 +0800

    modify 042

--- C++/042. Trapping Rain Water/main.cpp
+++ C++/042. Trapping Rain Water/main.cpp
@@ -5,46 +5,19 @@ using namespace std;
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = int(height.size());
+        auto n = int(height.size());
+        if (n < 3)
+            return 0;
+        vector<int> left (n, height[0]);
+        vector<int> right(n, height[n - 1]);
+        for (int i = 1; i < n; i++)
+            left[i] = max(height[i], left[i - 1]);
+        for (int i = n - 2; i >= 0; i--)
+            right[i] = max(height[i], right[i + 1]);
         int ans = 0;
         for (int i = 0; i < n; i++) {
-            if (i + 1 < n && height[i] <= height[i + 1])
-                continue;
-            int tmp = i + 1;
-            int h, l = 0;
-            bool tag = true;
-            for (int j = i + 1; j < n; j++) {
-                if (j + 1 < n && height[j + 1] > height[j])
-                    tag = false;
-                if (height[j] >= height[i]) {
-                    h = height[i];
-                    l = j - i - 1;
-                    tmp = j;
-                    break;
-                } else if (height[j] > height[tmp]) {
-                    h = height[j];
-                    l = j - i - 1;
-                    tmp = j;
-                }
-            }
-            if (tag)
-                break;
-            else if (l == 0)
-                continue;
-            int C = 0;
-            for (int k = i + 1; k < tmp; k++)
-                C = C + height[k];
-            ans = ans + (h * l - C);
-            i = tmp - 1;
+            ans += min(left[i], right[i]) - height[i];
         }
         return ans;
     }
 };
-
-int main() {
-    vector<int> height {2,1,0,2};
-    Solution so;
-    int ans = so.trap(height);
-    cout << ans << endl;
-    return 0;
-}
