0e7a257b510a92b5add17853d7f141206d5a21a5
qrs101 <qrs101@outlook.com>
2018-06-12 22:01:02 +0800

    commit 144 and 145

---
+++ C++/144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root == nullptr)
+            return res;
+        stack<TreeNode*> s;
+        s.push(root);
+        while (!s.empty()) {
+            TreeNode* p = s.top();
+            s.pop();
+            res.push_back(p->val);
+            if (p->right != nullptr)
+                s.push(p->right);
+            if (p->left != nullptr)
+                s.push(p->left);
+        }
+        return res;
+    }
+};
