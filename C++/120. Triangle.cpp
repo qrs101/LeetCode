641822f3feacdfaaa937a37d181384b8a62887f0
qrs101 <qrs101@outlook.com>
2018-07-07 14:17:39 +0800

    update c++ 120

--- C++/120. Triangle.cpp
+++ C++/120. Triangle.cpp
@@ -5,24 +5,13 @@ using namespace std;
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = int(triangle.size());
-        vector<vector<int>> dp (n, vector<int>(n, 0));
-        dp[0][0] = triangle[0][0];
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (j == 0)
-                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
-                else if (j == i)
-                    dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
-                else {
-                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
-                }
+        auto n = int(triangle.size());
+        vector<int> res (triangle[n - 1].begin(), triangle[n - 1].end());
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                res[j] = min(res[j], res[j + 1]) + triangle[i - 1][j];
             }
         }
-        int ans = dp[n - 1][0];
-        for (int i = 1; i < n; i++)
-            if (ans > dp[n - 1][i])
-                ans = dp[n - 1][i];
-        return ans;
+        return res[0];
     }
 };
