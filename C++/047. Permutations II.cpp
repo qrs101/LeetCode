676df9307fbc9a433fa9fc2bc0f47172d76fd7ee
qrs101 <qrs101@outlook.com>
2018-08-04 16:33:11 +0800

    go 046 047

---
+++ C++/047. Permutations II.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> permuteUnique(std::vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        std::vector<std::vector<int>> res;
+        while (true) {
+            res.emplace_back(nums);
+            int i, j, tmp;
+            for (i = int(nums.size()) - 2; i >= 0 && nums[i] >= nums[i + 1]; i--);
+            if (i < 0)
+                break;
+            for (j = i + 1; j < int(nums.size()) && nums[j] > nums[i]; j++);
+            tmp = nums[i];
+            nums[i] = nums[j - 1];
+            nums[j - 1] = tmp;
+            for (i = i + 1, j = int(nums.size()) - 1; i < j; i++, j--) {
+                tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+        }
+        return res;
+    }
+};
---
+++ Go/047. Permutations II.go
@@ -0,0 +1,24 @@
+package main
+
+import "sort"
+
+func permuteUnique(nums []int) [][]int {
+	sort.Ints(nums)
+	res := make([][]int, 0, 0)
+	for true {
+		tmp := make([]int, len(nums), len(nums))
+		copy(tmp, nums)
+		res = append(res, tmp)
+		var i, j int
+		for i = len(nums) - 2; i >= 0 && nums[i] >= nums[i + 1]; i-- {}
+		if i < 0 {
+			break
+		}
+		for j = i + 1; j < len(nums) && nums[j] > nums[i]; j++ {}
+		nums[i], nums[j - 1] = nums[j - 1], nums[i]
+		for i, j = i + 1, len(nums) - 1; i < j; i, j = i + 1, j - 1 {
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+	}
+	return res
+}
