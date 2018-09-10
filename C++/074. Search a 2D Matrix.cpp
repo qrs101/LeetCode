0b7876e5bfce5c502f525f490782f7277e91a923
qrs101 <qrs101@outlook.com>
2018-09-10 18:38:40 +0800

    modify c++ 074 075

--- C++/074. Search a 2D Matrix.cpp
+++ C++/074. Search a 2D Matrix.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (matrix.size() == 0 || matrix[0].size() == 0 || target < matrix[0][0])
+    bool searchMatrix(std::vector<std::vector<int>>& matrix, int target) {
+        if (matrix.size() == 0 || matrix[0].size() == 0)
             return false;
-        for (int i = 1; i < int(matrix.size()); i++) {
-            if (target >= matrix[i][0])
-                continue;
-            for (int j = 0; j < int(matrix[i - 1].size()); j++) {
-                if (matrix[i - 1][j] == target)
-                    return true;
+        auto m = int(matrix.size()), n = int(matrix[0].size());
+        for (int i = 0; i < m; i++) {
+            if (target >= matrix[i][0] && target <= matrix[i][n - 1]) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == target)
+                        return true;
+                }
             }
-            return false;
         }
-        for (auto i = int(matrix.size()) - 1, j = 0; j < matrix[i].size(); j++)
-            if (matrix[i][j] == target)
-                return true;
         return false;
     }
 };
--- C++/075. Sort Colors.cpp
+++ C++/075. Sort Colors.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int i, j, k;
-        for (i = 0; i < nums.size() && nums[i] == 0; i++);
-        for (j = i; j < nums.size() && nums[j] == 1; j++);
-        for (k = int(nums.size()) - 1; k >= j && nums[k] == 2; k--);
-        while (j <= k) {
-            if (nums[j] == 1)
-                j++;
-            else if (nums[j] == 0) {
-                int tmp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = tmp;
-                i++;
-                j++;
-            } else if (nums[j] == 2) {
-                int tmp = nums[j];
-                nums[j] = nums[k];
-                nums[k] = tmp;
-                k--;
+    void sortColors(std::vector<int>& nums) {
+        int loc_0 = 0, loc_2 = int(nums.size()) - 1;
+        for (int i = 0; i <= loc_2; i++) {
+            if (nums[i] == 0) {
+                swap(nums, loc_0, i);
+                loc_0++;
+            } else if (nums[i] == 2) {
+                swap(nums, loc_2, i);
+                i--;
+                loc_2--;
             }
         }
+    }
+
+    void swap(std::vector<int>& nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 };
