9aae38947bdbd52b022795aba577cf7c2c04b123
qrs101 <qrs101@outlook.com>
2018-10-18 01:12:36 +0800

    modify c++ 088 090

--- C++/088. Merge Sorted Array.cpp
+++ C++/088. Merge Sorted Array.cpp
@@ -5,33 +5,13 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0;
-        while (i < m && n > 0) {
-            if (nums1[i] >= nums2[0]) {
-                for (int k = m; k > i; k--)
-                    nums1[k] = nums1[k - 1];
-                nums1[i] = nums2[0];
-                nums2.erase(nums2.begin());
-                n--;
-                m++;
+        for (int i = 0, j; i < n; i++) {
+            int num = nums2[i];
+            for (j = 0; j < m + i && nums1[j] <= num; j++);
+            for (int k = m + i - 1; k >= j; k--) {
+                nums1[k + 1] = nums1[k];
             }
-            i++;
-        }
-        while (n > 0) {
-            nums1[i] = nums2[0];
-            nums2.erase(nums2.begin());
-            i++;
-            n--;
+            nums1[j] = num;
         }
     }
 };
-
-int main() {
-    vector<int> nums1 {0};
-    vector<int> nums2 {1};
-    Solution so;
-    so.merge(nums1, 0, nums2, 1);
-    for (auto i : nums1)
-        cout << i << endl;
-    return 0;
-}
--- C++/090. Subsets II.cpp
+++ C++/090. Subsets II.cpp
@@ -6,66 +6,36 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res{vector<int>{}};
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        for (int i = 0; i <= int(nums.size()); i++) {
-            vector<vector<int>> tmp = findk(nums, i);
-            ans.insert(ans.end(), tmp.begin(), tmp.end());
+        for (int i = 1; i <= int(nums.size()); i++) {
+            vector<vector<int>> tmp = findSubsetsWithLengthK(nums, i);
+            res.insert(res.end(), tmp.begin(), tmp.end());
         }
-        return ans;
+        return res;
     }
 
-    vector<vector<int>> findk(vector<int>& nums, int k) {
-        vector<vector<int>> ans;
-        if (k == 0) {
-            ans.push_back(vector<int>());
-            return ans;
+    vector<vector<int>> findSubsetsWithLengthK(vector<int> nums, int k) {
+        vector<vector<int>> res;
+        if (nums.size() < k)
+            return res;
+        if (nums.size() == k) {
+            res.emplace_back(nums);
+            return res;
         }
-        if (k > nums.size())
-            return ans;
-        if (k == nums.size()) {
-            ans.push_back(nums);
-            return ans;
-        }
-        if (k == 1) {
-            if (nums.size() >= 1)
-                ans.push_back(vector<int>(1, nums[0]));
-            for (int i = 1; i < nums.size(); i++) {
-                if (nums[i] != nums[i - 1])
-                    ans.push_back(vector<int>(1, nums[i]));
+        int loc = 1;
+        for (; loc < nums.size() && nums[loc] == nums[loc - 1]; loc++);
+        vector<int> _nums(nums.begin() + loc, nums.end());
+        for (int i = 0; i <= loc; i++) {
+            vector<vector<int>> tmp = findSubsetsWithLengthK(_nums, k - i);
+            if (tmp.size() == 0)
+                continue;
+            for (auto &subset : tmp) {
+                for (int j = 0; j < i; j++)
+                    subset.insert(subset.begin(), nums[0]);
             }
-            return ans;
+            res.insert(res.end(), tmp.begin(), tmp.end());
         }
-        vector<int> num(nums);
-        int t = 1;
-        for (int i = int(num.size()) - 2; i >= 0 && num[i] == num[num.size() - 1]; i--) {
-            t++;
-            num.erase(num.end() - 1);
-        }
-        int n = num[num.size() - 1];
-        num.erase(num.end() - 1);
-        for (int i = 0; i <= t; i++) {
-            vector<vector<int>> tmp = findk(num, k - i);
-            for (auto& j : tmp) {
-                for (int h = 0; h < i; h++)
-                    j.push_back(n);
-            }
-            ans.insert(ans.end(), tmp.begin(), tmp.end());
-            if (k - i == 0)
-                break;
-        }
-        return ans;
+        return res;
     }
 };
-
-int main() {
-    vector<int> nums {1,2,2};
-    Solution so;
-    vector<vector<int>> ans = so.findk(nums, 3);
-    for (auto i : ans) {
-        for (auto j : i)
-            cout << j << " ";
-        cout << "#" << endl;
-    }
-    return 0;
-}
